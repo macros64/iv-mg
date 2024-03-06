1643c453f189b80e6ffc6dbf707c03ec56b39d4d
macros <macros64@gmail.com>
2024-03-07 00:23:39 +0400

    cases list and new case page was adjusted

--- force-app/main/default/lwc/mrCasePage/mrCasePage.js
+++ force-app/main/default/lwc/mrCasePage/mrCasePage.js
@@ -2,5 +2,6 @@ import { LightningElement, api } from 'lwc';
 
 export default class MrCasePage extends LightningElement {
     @api recordId;
+    @api mode = 'view';
     fields = ['Region__c', 'Manufacture__c', 'Inspection_Types__c', 'Desired_inspection_start__c'];
 }
--- force-app/main/default/lwc/mrCasesListPage/mrCasesListPage.js
+++ force-app/main/default/lwc/mrCasesListPage/mrCasesListPage.js
@@ -1,10 +1,22 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
+
 import getCases from '@salesforce/apex/MGCasePageController.getCases';
 
-export default class MrCasesListPage extends LightningElement {
+export default class MrCasesListPage extends NavigationMixin(LightningElement) {
     isLoading = false;
     cases = [];
-    columns = ['Subject', 'CreatedDate', 'Region__c', 'Manufacture__c'];
+    columns = [
+        { 
+            label: 'Case Number', fieldName: 'ExperienceUrl__c', type: 'url',
+            typeAttributes: { label: { fieldName: 'CaseNumber' } }
+        },
+        { label: 'Case Subject', fieldName: 'Subject', type: 'text' },
+        { label: 'Created', fieldName: 'CreatedDate', type: 'date' },
+        { label: 'Status', fieldName: 'Status', type: 'text' },
+        { label: 'Region', fieldName: 'Region__c', type: 'text' },
+        { label: 'Place', fieldName: 'Manufacture__c', type: 'text' }
+    ];
 
     connectedCallback() {
         this.isLoading = true;
@@ -13,8 +25,17 @@ export default class MrCasesListPage extends LightningElement {
             this.cases = res;
         })
         .catch(err => { console.log(err); })
-        .fially(() => {
+        .finally(() => {
             this.isLoading = false;
-        })
+        });
+    }
+
+    newCase() {
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'New_Case__c' // TODO: move to attribute
+            }
+        });
     }
 }
