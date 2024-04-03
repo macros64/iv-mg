3e76e4ff530fc1d504ba6c0fcf8b6defd661bcba
macros <macros64@gmail.com>
2024-04-03 21:45:29 +0400

    approval process e2e ready

--- force-app/main/default/lwc/mrCasePage/mrCasePage.js
+++ force-app/main/default/lwc/mrCasePage/mrCasePage.js
@@ -1,10 +1,13 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from "lightning/uiRecordApi";
+import toast from 'lightning/toast';
+import { NavigationMixin } from "lightning/navigation";
 import submitCase from '@salesforce/apex/MGSubmitCaseController.submitCase';
 
-export default class MrCasePage extends LightningElement {
+export default class MrCasePage extends NavigationMixin(LightningElement) {
     @api recordId;
     @api mode = 'view';
+    isLoading = false;
     fields = ['Region__c', 'Manufacture__c', 'Inspection_Types__c', 'Desired_inspection_start__c', 'InspectionAddress__c'];
 
     get viewMode() {
@@ -29,12 +32,44 @@ export default class MrCasePage extends LightningElement {
     get canSubmit() {
         return this.ourCase?.data?.fields.Status.value == 'Draft';
     }
+    get canEdit() {
+        return this.ourCase?.data?.fields.Status.value == 'Draft';
+    }
 
     editClick() {
         this.mode = 'edit';
     }
 
     submitClick() {
-        submitCase({ caseId : this.recordId });
+        this.isLoading = true;
+        submitCase({ caseId : this.recordId })
+            .then(() => {
+                toast.show({
+                    label: 'Case submitted',
+                    variant: 'success'
+                }, this);
+
+                this[NavigationMixin.Navigate]({
+                    type: 'comm__namedPage',
+                    attributes: {
+                        name: 'My_open_cases__c' // TODO: move to attribute
+                    }
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                toast.show({
+                    label: 'Error',
+                    message: err.body?.message,
+                    variant: 'error'
+                }, this);
+            })
+            .finally(() => { 
+                this.isLoading = false; 
+            });
+    }
+
+    formLoad(evt) {
+        console.log('form load');
     }
 }
--- force-app/main/default/triggers/CaseTrigger.trigger
+++ force-app/main/default/triggers/CaseTrigger.trigger
@@ -3,6 +3,8 @@ trigger CaseTrigger on Case (before insert, before update) {
     for(Case c : Trigger.new) {
         if(c.RecordTypeId == rtId) {
             c.Subject = c.Region__c + ' - ' + c.Manufacture__c;
+            //Case oldCase = Trigger.old == null ? null : Trigger.oldMap.get(c.Id);
+            //if(oldCase != null && oldCase.Status != c.Status && c.Status == 'Draft') c.OwnerId = c.CreatedById; customer community can't be owner
         }
     }
 }
